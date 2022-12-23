6a987d4977470a3c7a10cd2e5284e3fa4eeec3f1
Andreas Fuchs <asf@boinkor.net>
2022-12-23 15:20:15 -0500

    Use dhat to improve the memory leak tests
    This makes them more precise and platform-independent; the allocator
    provided by dhat allows for a far smaller margin of error (just a tiny
    number of kilobytes, rather than 10MB).

--- governor/tests/memory_leaks.rs
+++ governor/tests/memory_leaks.rs
@@ -1,88 +1,95 @@
 #![cfg(feature = "std")]
 
-// This test uses procinfo, so can only be run on Linux.
-extern crate libc;
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
 
 use all_asserts::*;
 use governor::{Quota, RateLimiter};
 use nonzero_ext::*;
+use serial_test::serial;
+use std::hint::black_box;
 use std::sync::Arc;
 use std::thread;
 
-fn resident_memory_size() -> i64 {
-    let mut out: libc::rusage = unsafe { std::mem::zeroed() };
-    assert!(unsafe { libc::getrusage(libc::RUSAGE_SELF, &mut out) } == 0);
-    out.ru_maxrss
-}
-
-const LEAK_TOLERANCE: i64 = 1024 * 1024 * 10;
+const LEAK_TOLERANCE: usize = 2048;
 
 struct LeakCheck {
-    usage_before: i64,
     n_iter: usize,
-}
-
-impl Drop for LeakCheck {
-    fn drop(&mut self) {
-        let usage_after = resident_memory_size();
-        assert_le!(usage_after, self.usage_before + LEAK_TOLERANCE);
-    }
+    #[allow(dead_code)]
+    profiler: dhat::Profiler,
 }
 
 impl LeakCheck {
     fn new(n_iter: usize) -> Self {
         LeakCheck {
             n_iter,
-            usage_before: resident_memory_size(),
+            profiler: dhat::Profiler::builder().testing().build(),
+        }
+    }
+}
+
+impl Drop for LeakCheck {
+    fn drop(&mut self) {
+        let stats = dhat::HeapStats::get();
+        assert_le!(stats.curr_bytes, LEAK_TOLERANCE);
+    }
+}
+
+#[test]
+#[serial]
+fn memleak_gcra() {
+    let check = LeakCheck::new(500_000);
+    {
+        let bucket = RateLimiter::direct(Quota::per_second(nonzero!(1_000_000u32)));
+
+        for _i in 0..check.n_iter {
+            drop(black_box(bucket.check()));
         }
     }
 }
 
 #[test]
-fn memleak_gcra() {
-    let bucket = RateLimiter::direct(Quota::per_second(nonzero!(1_000_000u32)));
-
-    let leak_check = LeakCheck::new(500_000);
-
-    for _i in 0..leak_check.n_iter {
-        drop(bucket.check());
-    }
-}
-
-#[test]
+#[serial]
 fn memleak_gcra_multi() {
-    let bucket = RateLimiter::direct(Quota::per_second(nonzero!(1_000_000u32)));
-    let leak_check = LeakCheck::new(500_000);
+    let check = LeakCheck::new(500_000);
+    {
+        let bucket = RateLimiter::direct(Quota::per_second(nonzero!(1_000_000u32)));
 
-    for _i in 0..leak_check.n_iter {
-        drop(bucket.check_n(nonzero!(2u32)));
+        for _i in 0..check.n_iter {
+            drop(black_box(bucket.check_n(nonzero!(2u32))));
+        }
     }
 }
 
 #[test]
+#[serial]
 fn memleak_gcra_threaded() {
-    let bucket = Arc::new(RateLimiter::direct(Quota::per_second(nonzero!(
-        1_000_000u32
-    ))));
-    let leak_check = LeakCheck::new(5_000);
+    let check = LeakCheck::new(5_000);
+    {
+        let bucket = Arc::new(RateLimiter::direct(Quota::per_second(nonzero!(
+            1_000_000u32
+        ))));
 
-    for _i in 0..leak_check.n_iter {
-        let bucket = Arc::clone(&bucket);
-        thread::spawn(move || {
-            assert_eq!(Ok(()), bucket.check());
-        })
-        .join()
-        .unwrap();
+        for _i in 0..check.n_iter {
+            let bucket = Arc::clone(&bucket);
+            thread::spawn(move || {
+                assert_eq!(Ok(()), bucket.check());
+            })
+            .join()
+            .unwrap();
+        }
     }
 }
 
 #[test]
+#[serial]
 fn memleak_keyed() {
-    let bucket = RateLimiter::keyed(Quota::per_second(nonzero!(50u32)));
+    let check = LeakCheck::new(500_000);
+    {
+        let bucket = RateLimiter::keyed(Quota::per_second(nonzero!(50u32)));
 
-    let leak_check = LeakCheck::new(500_000);
-
-    for i in 0..leak_check.n_iter {
-        drop(bucket.check_key(&(i % 1000)));
+        for i in 0..check.n_iter {
+            drop(black_box(bucket.check_key(&(i % 1000))));
+        }
     }
 }
